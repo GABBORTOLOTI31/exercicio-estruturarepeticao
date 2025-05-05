f7cfab37c53728ff725d59a68c35070f6de2c66c
Gabriel Bortoloti <gabrielbortoloti2016@gmail.com>
2025-05-04 23:28:43 -0300

    exercicioSatisfacao

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,27 @@
-function exe0() {
+function exesatisfacao() {
 
     let nota // guarda notas
     let conta = 1 // conta de um a seis
     let soma = 0
+    let insatisfeitos = 0
+    let satisfeitos = 0
+
+
     while (conta <= 6 ){
         nota= Number(prompt(`informe a nota ${conta}`))
         conta++ // conta = conta + 1 
         soma = soma + nota
+
+    if (nota >= 8) {  // se nota for maior/igual 8
+
+        satisfeito++  // = satisfeito +1
+    } 
+    else if  (nota <= 5) { // = senao se 
+
+        insatisfeitos++ // = insatisfeito +1
+    }
+        conta++  // = conta + 1 
+
     }
    
 
@@ -14,8 +29,10 @@ function exe0() {
     //soma das notas
 
     //let soma = nota1 + nota2 + nota3 + nota4 + nota5 + nota6
-    let media = soma / 6 
+    let media = soma / 10 
 
     alert(`A media das notas ${media.toFixed(1)}`)
+    alert(`Total de pessoas satisfeitas : ${satisfeitos}`) 
+    alert(`Total de pessoas insatisfeitas : ${insatisfeitos}`)
 }
     
